40eac3da8171efa6ea6ddd63469f3663f4a297b2
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-01-30 15:31:10 +0100

    removed placeholder code and added array "pokemonList" with 6 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world!');
-let favoriteFood = 'spaghetti';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Lapras', pokedexNumber: 31, height: 250, weight: 220, type: ['water', 'ice']},
+    {name: 'Dragonite', pokedexNumber: 149, height: 220, weight: 210, type: ['dragon', 'flying']},
+    {name: 'Azumarill', pokedexNumber: 184, height: 80, weight: 28, type: ['water', 'fairy']},
+    {name: 'Charizard', pokedexNumber: 6, height: 170, weight: 90, type: ['flying', 'fire']},
+    {name: 'Butterfree', pokedexNumber: 12, height: 110, weight: 32, type: ['flying', 'bug'] },
+    {name: 'Aron', pokedexNumber: 304, height: 40, weight: 60, type: ['steel', 'rock']} 
+]

86832bc56f295ece0bcff8565ae5a776642d2ec9
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-02-19 22:41:06 +0100

    attempt to add favorites page (not working yet)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 
 let pokemonRepository = (function () {
     let pokemonList = [];
+    let favoritePokemonList = [];
+    let myFavorites = [];
+    let myFavoritesParsed = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
@@ -16,12 +19,15 @@ let pokemonRepository = (function () {
     function getAll() {
         return pokemonList;
     }
+    function getAllFavorites() {
+        return favoritePokemonList;
+    }
 
     function addListItem(pokemon) {
-        let pokemonUl = document.querySelector('.pokemon-list'); //pulls from Unordered list
-        let listItem = document.createElement('li'); //creates each LI element
-        listItem.classList.add('each-pokemon'); //adds class "each-pokemon" to each LI element
-        listItem.classList.add('list-group-item'); //adds class "list-group-item" to each LI element for bootstrap
+        let pokemonUnorderedList = $('#pokemon-list'); //pulls from Unordered list
+        let listItem = $('<li></li>'); //creates each LI element
+        listItem.addClass('each-pokemon'); //adds class "each-pokemon" to each LI element
+        listItem.addClass('list-group-item'); //adds class "list-group-item" to each LI element for bootstrap
         let button = document.createElement('button'); //creates button for pokemon
         button.classList.add('btn');
         button.classList.add('btn-light');//adds button classes for bootstrap
@@ -29,16 +35,47 @@ let pokemonRepository = (function () {
         button.classList.add('pokemon-button');//adds class 'pokemon-button' to each button
         button.setAttribute('data-toggle', 'modal');
         button.setAttribute('data-target', '#modal-container')
-        listItem.appendChild(button);//adds buttons to LIs
-        pokemonUl.appendChild(listItem);//adds LIs to UL
+        listItem.append(button);//adds buttons to LIs
+        pokemonUnorderedList.append(listItem);//adds LIs to UL
 
         button.addEventListener('click', function () { //shows all details of a pokemon in console when clicked on
             showDetails(pokemon);
         });
 
     }
-     function showModal(title, height, weight, image) {
-       
+    function addListItemFavorites(pokemon) {
+        let pokemonUnorderedList = $('#pokemon-list-favorites'); //pulls from Unordered list
+        let listItem = $('<li></li>'); //creates each LI element
+        listItem.addClass('each-pokemon'); //adds class "each-pokemon" to each LI element
+        listItem.addClass('list-group-item'); //adds class "list-group-item" to each LI element for bootstrap
+        let button = document.createElement('button'); //creates button for pokemon
+        button.classList.add('btn');
+        button.classList.add('btn-light');//adds button classes for bootstrap
+        button.innerText = pokemon.name;//adds pokemon name to each button
+        button.classList.add('pokemon-button');//adds class 'pokemon-button' to each button
+        button.setAttribute('data-toggle', 'modal');
+        button.setAttribute('data-target', '#modal-container')
+        listItem.append(button);//adds buttons to LIs
+        pokemonUnorderedList.append(listItem);//adds LIs to UL
+
+        button.addEventListener('click', function () { //shows all details of a pokemon in console when clicked on
+            showDetailsFavorites(pokemon);
+        });
+
+        
+
+    }
+
+    function addFavorites(pokemon) {
+        myFavorites.push(pokemon);
+        localStorage.setItem('myFavoritesString', JSON.stringify(myFavorites));
+        myFavoritesParsed= JSON.parse(localStorage.getItem('myFavoritesString'));
+        console.log(myFavoritesParsed);
+        alert('Pokemon added to favorites!');
+        
+    }
+    function showModal(title, height, weight, image, pokemon) {
+
         let modalTitle = document.querySelector('.modal-title');
         modalTitle.innerText = '';
         modalTitle.innerText = title;
@@ -61,17 +98,57 @@ let pokemonRepository = (function () {
         modal.appendChild(heightElement);
         modal.appendChild(weightElement);
         modal.appendChild(imageElement);
-        
 
 
+        let favoritePokemon = document.querySelector('.favorite-pokemon');
+        favoritePokemon.addEventListener('click', function () {
+            addFavorites(pokemon);
+        }
+        )
 
-     }
+    }
+
+    function showModalFavorites(title, height, weight, image) {
+
+        let modalTitle = document.querySelector('.modal-title');
+        modalTitle.innerText = '';
+        modalTitle.innerText = title;
+        modalTitle.classList.add('modal-element');
+        let modal = document.querySelector('.modal-body');
+        modal.innerText = ''
+        let heightElement = document.createElement('p');
+        heightElement.innerText = 'height: ' + height;
+        heightElement.classList.add('modal-element');
+        let weightElement = document.createElement('p');
+        weightElement.innerText = 'weight: ' + weight;
+        weightElement.classList.add('modal-element');
+
+        //add image
+        let imageElement = document.createElement('img');
+        imageElement.src = image;
+        imageElement.classList.add('modal-element');
+
+
+        modal.appendChild(heightElement);
+        modal.appendChild(weightElement);
+        modal.appendChild(imageElement);
+
+
+    }
    
+
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
-            showModal(pokemon.name, pokemon.height, pokemon.weight, pokemon.imageUrl);
+            showModal(pokemon.name, pokemon.height, pokemon.weight, pokemon.imageUrl, pokemon);
         });
     }
+
+    function showDetailsFavorites(pokemon) {
+        loadDetails(pokemon).then(function () {
+            showModalFavorites(pokemon.name, pokemon.height, pokemon.weight, pokemon.imageUrl);
+        });
+    }
+    
 
     function loadList() {
         return fetch(apiUrl).then(function (response) {
@@ -83,6 +160,30 @@ let pokemonRepository = (function () {
                     detailsUrl: item.url
                 };
                 add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadListFavorites() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                if (myFavoritesParsed.indexOf(item.name) !== -1) {
+                    
+                    
+
+                    let pokemon = {
+                        name: item.name,
+                        detailsUrl: item.url
+                    };
+                    favoritePokemonList.push(pokemon);
+                }
+                else {
+                    console.log(myFavorites);
+                }
             });
         }).catch(function (e) {
             console.error(e);
@@ -111,10 +212,15 @@ let pokemonRepository = (function () {
 
     return {
         add: add,
+        addFavorites: addFavorites,
         getAll: getAll,
+        getAllFavorites: getAllFavorites,
         addListItem: addListItem,
+        addListItemFavorites: addListItemFavorites,
         showDetails: showDetails,
+        showDetailsFavorites: showDetailsFavorites,
         loadList: loadList,
+        loadListFavorites: loadListFavorites,
         loadDetails: loadDetails
     };
 })();
@@ -125,6 +231,16 @@ pokemonRepository.loadList().then(function () {
 
     });
 })
+
+pokemonRepository.loadListFavorites().then(function () {
+    pokemonRepository.getAllFavorites().forEach(function (pokemon) {
+        pokemonRepository.addListItemFavorites(pokemon);
+
+    });
+})
+
+
+
 
 
 

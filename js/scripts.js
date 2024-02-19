268cae313fe2e0e97bd830651875cb4b1a8e235b
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-02-19 18:04:15 +0100

    added bootstrap modal and nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -21,36 +21,30 @@ let pokemonRepository = (function () {
         let pokemonUl = document.querySelector('.pokemon-list'); //pulls from Unordered list
         let listItem = document.createElement('li'); //creates each LI element
         listItem.classList.add('each-pokemon'); //adds class "each-pokemon" to each LI element
+        listItem.classList.add('list-group-item'); //adds class "list-group-item" to each LI element for bootstrap
         let button = document.createElement('button'); //creates button for pokemon
+        button.classList.add('btn');
+        button.classList.add('btn-light');//adds button classes for bootstrap
         button.innerText = pokemon.name;//adds pokemon name to each button
         button.classList.add('pokemon-button');//adds class 'pokemon-button' to each button
+        button.setAttribute('data-toggle', 'modal');
+        button.setAttribute('data-target', '#modal-container')
         listItem.appendChild(button);//adds buttons to LIs
         pokemonUl.appendChild(listItem);//adds LIs to UL
 
         button.addEventListener('click', function () { //shows all details of a pokemon in console when clicked on
             showDetails(pokemon);
         });
-
+//this is where I started!
     }
-    function showModal(title, height, weight, image) {
-        let modalContainer = document.querySelector('#modal-container');
-
-        //clear all existing modal content
-
-        modalContainer.innerHTML = '';
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-
-        //add new modal content
-        let closeButtonElement = document.createElement('button');
-        closeButtonElement.classList.add('modal-close');
-        closeButtonElement.innerText = 'X';
-        //closes modal when clicked
-        closeButtonElement.addEventListener('click', hideModal);
-        //replaces title and text with dynamic content as called in function parameters
-        let titleElement = document.createElement('h1');
-        titleElement.innerText = title;
-        titleElement.classList.add('modal-element');
+     function showModal(title, height, weight, image) {
+       
+        let modalTitle = document.querySelector('.modal-title');
+        modalTitle.innerText = '';
+        modalTitle.innerText = title;
+        modalTitle.classList.add('modal-element');
+        let modal = document.querySelector('.modal-body');
+        modal.innerText = ''
         let heightElement = document.createElement('p');
         heightElement.innerText = 'height: ' + height;
         heightElement.classList.add('modal-element');
@@ -61,37 +55,18 @@ let pokemonRepository = (function () {
         //add image
         let imageElement = document.createElement('img');
         imageElement.src = image;
-        imageElement.classList.add('pokeImg');
+        imageElement.classList.add('modal-element');
 
 
-
-
-        //glue it all together!
-        modal.appendChild(closeButtonElement);
-        modal.appendChild(titleElement);
         modal.appendChild(heightElement);
         modal.appendChild(weightElement);
         modal.appendChild(imageElement);
+        
 
 
-        modalContainer.appendChild(modal);
 
-        modalContainer.classList.add('is-visible');
-
-        //if clicked outside the modal, modal closes
-        modalContainer.addEventListener('click', (e) => {
-            let target = e.target;
-            if (target === modalContainer) {
-                hideModal();
-            }
-        });
-    }
-
-    function hideModal() {
-        let modalContainer = document.querySelector('#modal-container');
-        modalContainer.classList.remove('is-visible');
-    }
-
+     }
+   
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
             showModal(pokemon.name, pokemon.height, pokemon.weight, pokemon.imageUrl);
